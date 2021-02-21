c6d31b63caec197443570442b98151f8c4d48f7e
Ryan Nguyen <hoanganh8507@gmail.com>
2021-02-21 06:49:22 -0600

    Test Jenkins trigger

---
+++ src/test/java/com/leszko/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,22 @@
+package com.leszko.calculator;
+
+import com.leszko.calculator.service.Calculator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum(){
+        assertEquals(5, calculator.sum(2,3));
+    }
+
+}
